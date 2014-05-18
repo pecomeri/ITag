51d4664c1f512e24c7d72a12f7cea4ece994d1b5
pecomeri <takashina.k520@gmail.com>
2014-05-19 05:57:49 +0900

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Bijin::Application.routes.draw do
   devise_for :users
+  root to: "image#index"
   resources :images
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
